051d0ae640e3b5726c3d0c2c5f1dbe445b7563c7
baomei1996 <qhal4686@gmail.com>
2023-09-05 23:45:44 +0900

    Feat: create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+export default function Page() {
+    return (
+        <div className="max-w-6xl mx-auto space-x-4">
+            <h1 className="test-2xl font-medium text-amber-600">About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quaerat, deleniti consequuntur doloremque possimus asperiores
+                magni incidunt fuga ad voluptatibus rerum aperiam. Id ipsam
+                placeat modi autem deserunt labore pariatur consequatur!
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Officiis optio, tenetur cupiditate suscipit mollitia quam cum
+                maxime non molestias beatae soluta animi perferendis dolore,
+                quibusdam est adipisci a nesciunt enim.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate pariatur saepe tenetur. Quis ex enim ratione
+                recusandae nobis quisquam saepe, architecto dolores omnis
+                quaerat odit, a accusamus aspernatur, maiores officiis?
+            </p>
+        </div>
+    );
+}
